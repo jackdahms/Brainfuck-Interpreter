fdea1e67a73321cf35aed872f22b86657d593fae
jack <jack.dahms@gmail.com>
2016-05-09 17:18:00 -0500

    source scrolls

--- src/com/jackdahms/BrainfuckInterpreter.java
+++ src/com/jackdahms/BrainfuckInterpreter.java
@@ -12,7 +12,9 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 public class BrainfuckInterpreter extends JPanel {
@@ -21,7 +23,7 @@ public class BrainfuckInterpreter extends JPanel {
 	static int FRAME_HEIGHT = 600;
 	
 	public static void main(String[] args) {
-		JFrame frame = new JFrame("Cave Generation");
+		JFrame frame = new JFrame("Brainfuck Interpreter");
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT + 30);
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -46,13 +48,17 @@ public class BrainfuckInterpreter extends JPanel {
 		SpringLayout layout = new SpringLayout();
 		setLayout(layout);
 
+		//cells, well the first twenty of them at least
+		
 		JLabel inputLabel = new JLabel("INPUT");
+		JLabel inputInfoLabel = new JLabel("(ignores white space)");
 		JLabel sourceLabel = new JLabel("SOURCE");
 		JLabel outputLabel = new JLabel("OUTPUT");
 		
-		JTextArea input = new JTextArea(1, 40);
-		JTextArea source = new JTextArea(18, 40);
-		JTextArea output = new JTextArea(1, 40);
+		JTextField input = new JTextField();
+		JTextArea sourceArea = new JTextArea();
+		JScrollPane sourceScroll = new JScrollPane(sourceArea);
+		JTextArea output = new JTextArea(2, 40);
 
 		JLabel memoryLabel = new JLabel("MEMORY");
 		
@@ -61,11 +67,16 @@ public class BrainfuckInterpreter extends JPanel {
 		layout.putConstraint(SpringLayout.WEST, inputLabel, 5, SpringLayout.WEST, this);
 		add(inputLabel);
 		
+		inputInfoLabel.setFont(new Font("monospace", Font.PLAIN, 12));
+		layout.putConstraint(SpringLayout.SOUTH, inputInfoLabel, 0, SpringLayout.SOUTH, inputLabel);
+		layout.putConstraint(SpringLayout.WEST, inputInfoLabel, 5, SpringLayout.EAST, inputLabel);
+		add(inputInfoLabel);
+		
 		input.setFont(new Font("courier", Font.PLAIN, 20));
-		input.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-		input.setTabSize(4);
+		input.setBorder(BorderFactory.createLineBorder(new Color(150, 150, 150), 1));
 		layout.putConstraint(SpringLayout.NORTH, input, 5, SpringLayout.SOUTH, inputLabel);
 		layout.putConstraint(SpringLayout.WEST, input, 0, SpringLayout.WEST, inputLabel);
+		layout.putConstraint(SpringLayout.EAST, input, -5, SpringLayout.EAST, this);
 		add(input);
 		
 		sourceLabel.setFont(new Font("courier", Font.PLAIN, 20));
@@ -73,20 +84,21 @@ public class BrainfuckInterpreter extends JPanel {
 		layout.putConstraint(SpringLayout.WEST, sourceLabel, 0, SpringLayout.WEST, inputLabel);
 		add(sourceLabel);
 		
-		source.setFont(new Font("courier", Font.PLAIN, 20));
-		source.setBorder(BorderFactory.createLineBorder(Color.black, 1));
-		source.setTabSize(4);
-		layout.putConstraint(SpringLayout.NORTH, source, 5, SpringLayout.SOUTH, sourceLabel);
-		layout.putConstraint(SpringLayout.WEST, source, 0, SpringLayout.WEST, sourceLabel);
-		add(source);
+		sourceArea.setFont(new Font("courier", Font.PLAIN, 20));
+		sourceArea.setTabSize(4);
+		sourceArea.setLineWrap(true);
+		sourceScroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		layout.putConstraint(SpringLayout.NORTH, sourceScroll, 5, SpringLayout.SOUTH, sourceLabel);
+		layout.putConstraint(SpringLayout.WEST, sourceScroll, 0, SpringLayout.WEST, sourceLabel);
+		add(sourceScroll);
 		
 		outputLabel.setFont(new Font("courier", Font.PLAIN, 20));
-		layout.putConstraint(SpringLayout.NORTH, outputLabel, 5, SpringLayout.SOUTH, source);
+		layout.putConstraint(SpringLayout.NORTH, outputLabel, 5, SpringLayout.SOUTH, sourceScroll);
 		layout.putConstraint(SpringLayout.WEST, outputLabel, 0, SpringLayout.WEST, inputLabel);
 		add(outputLabel);
 		
 		output.setFont(new Font("courier", Font.PLAIN, 20));
-		output.setBorder(BorderFactory.createLineBorder(Color.black, 1));
+		output.setBorder(BorderFactory.createLineBorder(new Color(150, 150, 150), 1));
 		output.setTabSize(4);
 		layout.putConstraint(SpringLayout.NORTH, output, 5, SpringLayout.SOUTH, outputLabel);
 		layout.putConstraint(SpringLayout.WEST, output, 0, SpringLayout.WEST, outputLabel);
@@ -95,7 +107,6 @@ public class BrainfuckInterpreter extends JPanel {
 		layout.putConstraint(SpringLayout.SOUTH, memoryLabel, -5, SpringLayout.NORTH, this);
 		layout.putConstraint(SpringLayout.WEST, memoryLabel, 0, SpringLayout.WEST, this);
 		add(memoryLabel);
-		
 	}
 	
 }

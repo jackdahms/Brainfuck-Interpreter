69494f6e27d20d72ef84a0ee4b82bd7bbcaa82b0
jack <jack.dahms@gmail.com>
2016-05-10 22:50:34 -0500

    turned off gridlines

--- src/com/jackdahms/BrainfuckInterpreter.java
+++ src/com/jackdahms/BrainfuckInterpreter.java
@@ -8,6 +8,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class BrainfuckInterpreter extends Application {
@@ -21,11 +22,11 @@ public class BrainfuckInterpreter extends Application {
         
 		GridPane grid = new GridPane();
 		grid.setAlignment(Pos.CENTER);
-		grid.setHgap(5);
+		grid.setHgap(10);
 		grid.setVgap(5);
-		grid.setPadding(new Insets(5, 5, 5, 5));
+		grid.setPadding(new Insets(5, 10, 5, 10));
 		
-		grid.setGridLinesVisible(true);
+//		grid.setGridLinesVisible(true);
 		
 		Label inputLabel = new Label("INPUT");
 		GridPane.setHgrow(inputLabel, Priority.ALWAYS);
@@ -33,14 +34,23 @@ public class BrainfuckInterpreter extends Application {
 		
 		TextArea input = new TextArea();
 		input.setMaxHeight(40);
-		grid.add(input, 0, 1);
+		VBox vbInput = new VBox();
+		vbInput.setAlignment(Pos.TOP_CENTER);
+		vbInput.getChildren().add(input);
+		GridPane.setVgrow(vbInput, Priority.ALWAYS);
+		grid.add(vbInput, 0, 1);
 		
 		Label outputLabel = new Label("OUTPUT");
 		GridPane.setHgrow(outputLabel, Priority.ALWAYS);
 		grid.add(outputLabel, 1, 0);
 		
 		TextArea output = new TextArea();
-		grid.add(output, 1, 1);
+		output.setMaxHeight(40);
+		VBox vbOutput = new VBox();
+		vbOutput.setAlignment(Pos.TOP_CENTER);
+		vbOutput.getChildren().add(output);
+		GridPane.setVgrow(vbOutput, Priority.ALWAYS);
+		grid.add(vbOutput, 1, 1);
 
 		Scene scene = new Scene(grid, 1000, 600);
 		try {
